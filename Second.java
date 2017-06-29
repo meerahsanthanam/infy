76535ad912dd6d40e4bc0a34ac1d8335433602a4
meerahsanthanam <meerahsanthanam8@gmail.com>
2017-06-29 11:45:52 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,7 @@
+package newp;
+
+public class Second {
+public void greet(String s){
+	System.out.println("Hi"+s);
+}
+}
---
+++ first.java
@@ -0,0 +1,11 @@
+package newp;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Second s = new Second();
+s.greet("Meerah");
+	}
+
+}
